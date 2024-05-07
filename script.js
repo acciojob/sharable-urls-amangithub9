81ba05f9e63585b42048c2f6813aca9d981a0da2
Aman Gupta <115404057+amangithub9@users.noreply.github.com>
2024-05-07 13:20:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // your code here
+function changeHeading(){
+	let input1 = document.getElementById("name").value;
+	let input2 = document.getElementById("year").value;
+
+	let changeUrl = document.getElementById("url");
+	let innerHtml = `?name=${input1}&year=${input2}`
+	changeUrl.innerHtml += innerHtml;
+}
